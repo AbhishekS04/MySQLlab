003519729fe586a9c23b6eed086eb54dc6bd9d96
AbhishekS04 <abhishek23main@gmail.com>
2025-03-07 10:11:48 +0530

    Consolidate SQL queries into new assignment files and remove obsolete scripts

---
+++ ASSIGNMENT-5B.sql
@@ -0,0 +1,17 @@
+-- 11.     Calculate the average price of all movies where type is 'comedy' or 'thriller' and price is greater than or equal to 150.00.
+
+-- 12.	Display the invoice number and day on which customers were issued movies.
+
+-- 13.	Display the month (in alphabets) in which customers are supposed to return the movies.
+
+-- 14.	Display the issue-date in the format 'dd-month-yy'.
+
+-- For eg. 12-february-93.
+
+-- 15.	Find current date.
+
+--  16.       Find current date and time.
+
+--  17.       Find the date, 15 days after the current date.
+
+-- 18.	Find the number of days elapsed between the current date and the return date of the movie for all customers.
